ccfe6376ac5cdacf2ec5812506235edd7b12a435
tdnicola <tdnicola@gmail.com>
2020-02-25 12:20:17 -0500

    added additional models for cards and tasks. working on creating a user and having prepopulated tasks be generated.

--- model.js
+++ model.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
-const KanBanSchema = Schema({
+const UsersSchema = Schema({
     Password: { type: String, required: true},
     Email: { type: String, required: true},
-    Tasks: [],
+    Tasks: [{ type : mongoose.Schema.Types.ObjectId, ref: 'Cards' }],
 });
 
-const Tasks = mongoose.model('Tasks', KanBanSchema)
+const CardsSchema = Schema({
+    Cards: [{ type : mongoose.Schema.Types.ObjectId, ref: 'Tasks' }]
+});
+
+const TasksSchema = Schema({
+    Tasks: [String]
+});
+
+const Users = mongoose.model('Users', UsersSchema)
+const Cards = mongoose.model('Cards', CardsSchema)
+const Tasks = mongoose.model('Tasks', TasksSchema)
+
+module.exports.Users = Users
+module.exports.Cards = Cards
 module.exports.Tasks = Tasks
+
--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const Model = require('./model');
 
-const Tasks = Model.Tasks
+const Users = Model.Users;
+const Tasks = Model.Tasks;
+const Cards = Model.Cards;
 
 const app = express();
 
@@ -15,7 +17,7 @@ mongoose.connect('mongodb://localhost:27017/kanban', {useNewUrlParser: true});
 
 //get request, gives all users.
 app.get('/users', (req, res) => {
-    Tasks.find()
+    Users.find()
     .then(users => res.json(users))
     .catch((error) => {
         console.error(error);
@@ -24,8 +26,8 @@ app.get('/users', (req, res) => {
 });
 
 //get information on one user.
-app.get('/tasks/:Email', (req, res) => {
-    Tasks.findOne({ Email : req.params.Email})
+app.get('/users/:Email', (req, res) => {
+    Users.findOne({ Email : req.params.Email})
     .then((user) => {
         res.json(user)
     })
@@ -37,34 +39,35 @@ app.get('/tasks/:Email', (req, res) => {
 
 //create user. gives default todo,inprogress,completed tasks for new user.
 app.post('/users', ((req, res) => {
-    Tasks.findOne({ Email : req.body.email })
+    Users.findOne({ Email : req.body.email })
     .then((email) => {
         if (email) {
             res.status(400).send(req.body.email + ' Already Exists!' )
             console.log(req.body)
         } else {
             console.log(req.body)
-            Tasks
+            Users
             .create({
                 Email: req.body.email,
                 Password: req.body.password,
-                Tasks: [
-                    {
-                        "todo":[
-                            "start"
-                        ]
-                    },
-                    {
-                        "inprogress":[
-                            "moving along"
-                        ]
-                    },
-                    {
-                        "completed":[
-                            "done"
-                        ]
-                    }
-                ]
+                // Tasks: 
+                //     [
+                //         {
+                //             "todo":[
+                //                 "start"
+                //             ]
+                //         },
+                //         {
+                //             "inprogress":[
+                //                 "moving along"
+                //             ]
+                //         },
+                //         {
+                //             "completed":[
+                //                 "done"
+                //             ]
+                //         }
+                //     ]
             })
             .then((email) => {res.status(201).json(email) })
             .catch((error) => {
@@ -81,11 +84,21 @@ app.post('/users', ((req, res) => {
 
 //update tasks by email
 app.put('/tasks/:Email', (req, res) => {
-    Tasks.findOneAndUpdate({ Email: req.params.Email }), { $set : {
-        Email : req.body.Email,
-        Password: req.body.Password,
-    }}
-})
+    Users.updateOne({ Email: req.params.Email }, { $set : 
+    {
+        Email : req.body.email,
+        Password: req.body.password,
+    }},
+    { new : true})
+    .then((user) => { 
+        console.log(req.body, req.params);
+        res.status(201).json(user) 
+    })
+    .catch((error) => {
+        console.log(req.body);
+        res.status(500).send('error ' + error);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Connected on port 3000')
